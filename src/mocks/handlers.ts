b6bed6f253a17a0ce15994a2ecfa6364607c6b0a
Kieran Allen <kieran.allen@truelayer.com>
2021-10-07 18:02:13 +0100

    add mocks

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,24 @@
+import { rest } from 'msw'
+
+let favorites: number[] = []
+
+const storedFavorites = sessionStorage.getItem('favorites')
+
+if (storedFavorites) {
+  favorites = (JSON.parse(storedFavorites) as number[]) ?? []
+}
+
+export const handlers = [
+  rest.post<{ beerId: number }>('/favorite', (req, res, ctx) => {
+    favorites = favorites.some(f => f === req.body.beerId)
+      ? favorites.filter(f => f !== req.body.beerId)
+      : [...favorites, req.body.beerId]
+
+    sessionStorage.setItem('favorites', JSON.stringify(favorites))
+
+    return res(ctx.status(200), ctx.json(favorites))
+  }),
+  rest.get('/favorite', (_, res, ctx) => {
+    return res(ctx.status(200), ctx.json(favorites))
+  }),
+]
