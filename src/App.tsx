94efa78e7609456b1db99be70fd217be31757734
Kieran Allen <kieran.allen@truelayer.com>
2021-10-08 12:40:07 +0100

    add style to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import clsx from 'clsx'
 import { useState } from 'react'
 import { Form } from './components/Form'
-import { FormData } from './types/FormData'
+import type { FormData } from './types/FormData'
 
 export function App() {
   const [formData, setFormData] = useState<FormData>({ maxAbv: 4 })
 
   return (
-    <div className={clsx('card')}>
+    <div className={clsx('card', 'flex', 'flex-col', 'gap-4')}>
       <header>
-        <h1 className={clsx('text-xl', 'underline')}>🍺 Beerz</h1>
+        <h1 className={clsx('text-4xl', 'underline')}>🍺 Beerz</h1>
       </header>
       <Form formData={formData} setFormData={setFormData} />
     </div>
